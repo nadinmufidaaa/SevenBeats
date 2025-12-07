195d1a54927521733c938fead4aa4594e419e5d8
phaivalori12 <valorina64@gmail.com>
2025-12-07 22:22:10 +0800

    Add files via upload

---
+++ sevenbeatstube/src/GenreNode.java
@@ -0,0 +1,10 @@
+public class GenreNode {
+    String genre;
+    GenreNode left, right;
+
+    GenreNode(String g) {
+        this.genre = g;
+        this.left = null;
+        this.right = null;
+    }
+}
