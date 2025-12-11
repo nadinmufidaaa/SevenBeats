28038d1aa25bb13ad268c13dcd3c98db63dc6a7a
minemis001 <113583036+minemis001@users.noreply.github.com>
2025-12-11 23:59:41 +0800

    latest version of stack code

--- sevenbeatstube/src/Stack.java
+++ sevenbeatstube/src/Stack.java
@@ -1,32 +1,20 @@
 public class Stack {
     Node top;
-
     public void push(Video v) {
         Node baru = new Node(v);
         baru.next = top;
         top = baru;
     }
-
-    public Video pop() {
-        if (top == null) return null;
-        Video v = top.data;
-        top = top.next;
-        return v;
-    }
-
-    public boolean isEmpty() {
-        return top == null;
-    }
-
+    
     public void tampilkanHistory() {
         if (top == null) {
             System.out.println("Riwayat kosong!\n");
             return;
         }
-        System.out.println("Riwayat tontonan (terbaru di atas, max 5):");
+        System.out.println("Riwayat tontonan");
         Node temp = top;
         int count = 0;
-        while (temp != null && count < 5) {
+        while (temp != null) {
             System.out.println((count + 1) + ". " + temp.data.judul);
             temp = temp.next;
             count++;
